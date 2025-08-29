8e83cd8a2208cd1c385a84f82fac98709f7090cf
JosephWee <86550414+JosephWee@users.noreply.github.com>
2025-08-29 13:59:25 +0800

    Added ExampleUnitUnderTest.js.

---
+++ ExplicitContentDetectionWeb/wwwroot/UnitTests/SafeSearch/ExampleUnitUnderTest.js
@@ -0,0 +1,18 @@
+﻿// ExampleUnitUnderTest.js - The code to be tested
+export default class ExampleUnitUnderTest {
+    constructor() {
+        this.messages = [];
+    }
+
+    push(message) {
+        return this.messages.push(message);
+    }
+
+    pop() {
+        return this.messages.pop();
+    }
+
+    getCount() {
+        return this.messages.length;
+    }
+}
